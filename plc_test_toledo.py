1cacba1d8199f6c2033e0a26bb13debe1dbad307
Howard Roush <howard.roush@phoeniximaging.com>
2022-02-15 16:14:25 -0500

    Added in REAL-WORLD test in Toledo (plc_test_toledo.py), modified to read PLC tags using PYCOMM3 library, test sequence to read/write boolean tags, flips True/False set amount of times

---
+++ plc_test_toledo.py
@@ -0,0 +1,116 @@
+from pycomm3 import LogixDriver
+from pycomm3.cip.data_types import DINT, UINT
+import time
+
+with LogixDriver("120.123.230.39/0") as plc:
+            plc.open()
+            '''
+            print("PLC: ")
+            print(plc)
+            print()
+            print("PLC Info: ")
+            print(plc.info)
+            print()
+            #print("Tags(blob):")
+            #print(plc._tags) #prints tags
+
+            for tag in plc.tags:
+                print("Tags: ")
+                print(tag + ": ")
+                print()
+            '''
+            
+            tags = ['Program:CM080CA01.PorosityInspect.CAM01.O.LOAD_PROGRAM',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.START_PROGRAM',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.END_PROGRAM',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.ABORT_PROGRAM',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.RESET',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.HEARTBEAT',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.MASTER_PART',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.PART_TYPE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.PART_PROGRAM',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.PUN',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.MODULE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.PLANTCODE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.TIMESTAMP_MONTH',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.TIMESTAMP_DAY',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.TIMESTAMP_YEAR',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.TIMESTAMP_HOUR',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.TIMESTAMP_MINUTE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.O.TIMESTAMP_SECOND']
+
+            
+            #result = plc.read('Program:CM080CA01.PorosityInspect') #reading 1 field from PLC *VALID RESULTS
+            #result = plc.read('Program:CM080CA01.PorosityInspect.CAM01.O.LOAD_PROGRAM')
+            #print(result)
+
+            #print('GrobPLCToPhoenix Tags:')
+            for tag in tags:
+                result = plc.read(tag)
+                #print(f'{tag}: {result[1]} | {type(result[1])}')
+            #print()
+
+            tags = ['Program:CM080CA01.PorosityInspect.CAM01.I.READY',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.BUSY',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.DONE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.PASS',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.FAIL',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.HEARTBEAT',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.MASTER_PART',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.PART_TYPE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.PART_PROGRAM',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.PUN',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.MODULE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.PLANTCODE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.TIMESTAMP_MONTH',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.TIMESTAMP_DAY',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.TIMESTAMP_YEAR',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.TIMESTAMP_HOUR',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.TIMESTAMP_MINUTE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.TIMESTAMP_SECOND',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.DEFECT_NUMBER',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.DEFECT_SIZE',
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.DEFECT_ZONE']
+            #print('PhoenixToGrobPLC Tags:')
+            for tag in tags:
+                result = plc.read(tag)
+                #print(f'{tag}: {result[1]} | {type(result[1])}')
+            #print()
+
+            #toggle booleans True/False 3 times
+            for x in range(3):
+                plc_display_single = plc.read('Program:CM080CA01.PorosityInspect.CAM01.I.READY')
+                print(f'READY was: {plc_display_single[1]} {type(plc_display_single[1])}')
+                #flip TRUE/FALSE each run
+                if(plc_display_single[1] == 0):
+                    print('Writing READY = True')
+                    try:
+                        plc.write(('Program:CM080CA01.PorosityInspect.CAM01.I.READY', 1),
+                                  ('Program:CM080CA01.PorosityInspect.CAM01.I.BUSY', 1),
+                                  ('Program:CM080CA01.PorosityInspect.CAM01.I.DONE', 1),
+                                  ('Program:CM080CA01.PorosityInspect.CAM01.I.PASS', 1),
+                                  ('Program:CM080CA01.PorosityInspect.CAM01.I.FAIL', 1))
+                    except Exception as e:
+                        print(e)
+                    print('Write Complete!')
+                else:
+                    print('Writing READY = False')
+                    plc.write(('Program:CM080CA01.PorosityInspect.CAM01.I.READY', 0),
+                                  ('Program:CM080CA01.PorosityInspect.CAM01.I.BUSY', 0),
+                                  ('Program:CM080CA01.PorosityInspect.CAM01.I.DONE', 0),
+                                  ('Program:CM080CA01.PorosityInspect.CAM01.I.PASS', 0),
+                                  ('Program:CM080CA01.PorosityInspect.CAM01.I.FAIL', 0))
+                    
+                plc_display_booleans = plc.read('Program:CM080CA01.PorosityInspect.CAM01.I.READY',
+                                              'Program:CM080CA01.PorosityInspect.CAM01.I.BUSY',
+                                              'Program:CM080CA01.PorosityInspect.CAM01.I.DONE',
+                                              'Program:CM080CA01.PorosityInspect.CAM01.I.PASS',
+                                              'Program:CM080CA01.PorosityInspect.CAM01.I.FAIL')
+                for result in plc_display_booleans:
+                    print(f'{result[0]} : {result[1]}')
+                #print(f'READY now: {plc_display_single[1]}')
+
+                time.sleep(3) #artificial pause between flips
+
+            print('Closing PLC connection')
+            plc.close
--- trigger_livonia_TwoThreads.py
+++ trigger_livonia_TwoThreads.py
@@ -53,11 +53,11 @@ def TriggerKeyence(sock, item):
     # looping until '%Busy' == 0
     while(data != b'MR,+0000000000.000000\r'):
         # utilizing 'with' to use thread lock
+        message = 'MR,%Busy\r\n'
         with lock:
-            message = 'MR,%Busy\r\n'
             sock.sendall(message.encode())
             data = sock.recv(32)
-            print('TriggerKeyence: received "%s"' % data)
+        print('TriggerKeyence: received "%s"' % data)
         #print('Scanning...')
         time.sleep(.5)
 #END 'TriggerKeyence'
@@ -78,10 +78,14 @@ def ExtKeyence(sock, item, item_reset):
 # END 'ExtKeyence'
 
 # function to read in .csv file emulating plc from phoenix-101
-def csv_read():
+def csv_read(io):
     #csv file reader variable declaration
     #file = open('\\phoenix-101\homes\Howard.Roush\Drive\plc_dummy.csv') #Python doesn't like \\
-    file = open('//phoenix-101/homes/Howard.Roush/Drive/plc_dummy.csv')
+    #file = open('//phoenix-101/homes/Howard.Roush/Drive/plc_dummy.csv')
+    if io == 'o':
+        file = open('//phoenix-101/homes/Howard.Roush/Drive/phoenix_to_plc.csv')
+    if io == 'i':
+        file = open('//phoenix-101/homes/Howard.Roush/Drive/plc_to_phoenix.csv')
     csvreader = csv.reader(file)
 
     #creating two lists that will be combined in the dictionary 'plc_dict'
@@ -104,21 +108,52 @@ def csv_read():
     return plc_dict
 #END csv_read
 
+# Writes back to .csv (plc) with current tag values
+def csv_write(results):
+    header = []
+    values = []
+    #file = open('//phoenix-101/homes/Howard.Roush/Drive/plc_dummy.csv', 'w', newline='')
+    file = open('//phoenix-101/homes/Howard.Roush/Drive/phoenix_to_plc.csv', 'w', newline='')
+    csv_writer = csv.writer(file)
+
+    for key in results:
+        header.append(key)
+        values.append(results[key])
+
+    csv_writer.writerow(header)
+    csv_writer.writerow(values)
+
 
 #START main()
 def main():
 
     #reading in .csv plc emulator for initial values
-    csv_results = csv_read()
-    #print(csv_results)
+    #split into two .csv files, representing each direction of traffic between PLC and Phoenix
+    csv_results = csv_read('o') #holds PHOENIX tags / data
+    csv_results_plc = csv_read('i') #holds PLC (Grob) tags / data
 
-    print('Listening for \'START_PROGRAM\' = 1')
-    #reading .csv until START_PROGRAM goes high
-    while(csv_results['START_PROGRAM'] == 0):
-        csv_results = csv_read()
-        #print(csv_results)
-        time.sleep(1)
+    while(True):
+        print('Stage 1 : Listening for \'LOAD_PROGRAM\' = 1 from PLC')
+        #reading .csv until LOAD_PROGRAM goes high
+        while(csv_results_plc['LOAD_PROGRAM'] != 1):
+            csv_results_plc = csv_read('i')
+            #print(csv_results)
+            time.sleep(1)
 
+        print('PLC(LOAD_PROGRAM) went high!')
+        # Once PLC(LOAD_PROGRAM) goes high, mirror data and set Phoenix(READY) high, signifies end of "loading" process
+        csv_results['READY'] = 0 # LOAD is high, so READY goes low
+        csv_write(csv_results)
+        print('Dropping Phoenix(READY) low.')
+        print('!Mirroring Data!')
+        time.sleep(3) #artificial pause to see step happening in testing
+        print('Data Mirrored, Setting \'READY\' high')
+        csv_results['READY'] = 1
+        csv_write(csv_results)
+        time.sleep(3)
+
+
+    #BEGIN THREADING TRIGGER KEYENCE
     #declaring threads, does not run
     t1 = threading.Thread(target=TriggerKeyence, args=[sock, 'T1\r\n']) #thread1, passing in socket connection and 'T1' keyence command
     t2 = threading.Thread(target=ExtKeyence, args=[sock, 'TE,0\r\n', 'TE,1\r\n']) #thread2, uses 'TE,0' and 'TE,1' to cancel while scanning and reset to original state
