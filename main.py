eb642d2b4b358ee93026cd027f5eaa0d6bbcc5f4
Howard Roush <howard.roush@phoeniximaging.com>
2022-02-22 14:42:29 -0500

    Tested at Grob (Keyence was disconnected at the time), re-enabled Keyence socket to push to github, connects and reads PLC tags confirmed, requires actual cycle test with a connected Keyence (172.19.147.82:8500)

--- main.py
+++ main.py
@@ -14,96 +14,6 @@ This is the first testing thread for Elwema using threading and an all-in-one Py
 Progresses through the timing diagram based on tag values read off Elwema's PLC. Loads and Triggers Keyence.
 '''
 
-
-# global variable declarations, some are probably unnecessary(?)
-arrayOutTags = [
-    'LoadProgram',
-    'StartProgram',
-    'EndProgram',
-    'AbortProgram',
-    'Reset',
-    'PartType',
-    'PartProgram',
-    'ScanNumber',
-    'PUN{64}',
-    'GMPartNumber{8}',
-    'Module',
-    'PlantCode',
-    'Month',
-    'Day',
-    'Year',
-    'Hour',
-    'Minute',
-    'Second',
-    'QualityCheckOP110',
-    'QualityCheckOP120',
-    'QualityCheckOP130',
-    'QualityCheckOP140',
-    'QualityCheckOP150',
-    'QualityCheckOP310',
-    'QualityCheckOP320',
-    'QualityCheckOP330',
-    'QualityCheckOP340',
-    'QualityCheckOP360',
-    'QualityCheckOP370',
-    'QualityCheckOP380',
-    'QualityCheckOP390',
-    'QualityCheckScoutPartTracking',
-    #'KeyenceFltCode',
-    #'PhoenixFltCode'
-    ];
-
-tagKeys = []
-for tag in arrayOutTags:
-    tagKeys.append(tag.split("{")[0]) # delete trailiing { if it exists
-
-#order matters!
-tagCmds = [
-    'LoadProgram',
-    'EndProgram',
-    'StartProgram',
-    'AbortProgram',
-    'Reset'];
-
-tagDateTime = [
-    'Month',
-    'Day',
-    'Year',
-    'Hour',
-    'Minute',
-    'Second'];
-
-tagStringInt = [
-    'PUN',
-    'GMPartNumber'];
-
-tagInt = [
-    'PartType',
-    'PartProgram',
-    'ScanNumber',
-    #'KeyenceFltCode',
-    #'PhoenixFltCode'
-    ];
-
-tagChar = [
-    'Module',
-    'PlantCode',
-    'QualityCheckOP110',
-    'QualityCheckOP120',
-    'QualityCheckOP130',
-    'QualityCheckOP140',
-    'QualityCheckOP150',
-    'QualityCheckOP310',
-    'QualityCheckOP320',
-    'QualityCheckOP330',
-    'QualityCheckOP340',
-    'QualityCheckOP360',
-    'QualityCheckOP370',
-    'QualityCheckOP380',
-    'QualityCheckOP390',
-    'QualityCheckScoutPartTracking'
-        ];
-
 trigger_count = 0
 slow_count = 0
 longest_time = 0
@@ -112,16 +22,67 @@ current_stage = 0
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #sock.connect(('192.168.1.83', 8500)) # 'sock' is the connection variable used to communicate with the Keyence
-sock.connect(('172.19.145.80', 8500))
+sock.connect(('172.19.147.82', 8500)) # GROB address, Keyence head #1
 
 lock = threading.Lock()
 
-#single-shot read of all 'arrayOutTags' off PLC
+arrayOutTags = [
+    'LOAD_PROGRAM',
+    'START_PROGRAM',
+    'END_PROGRAM',
+    'ABORT_PROGRAM',
+    'RESET',
+    'PART_TYPE',
+    'PART_PROGRAM',
+    'PUN{64}',
+    'MODULE',
+    'PLANTCODE',
+    'TIMESTAMP_MONTH',
+    'TIMESTAMP_DAY',
+    'TIMESTAMP_YEAR',
+    'TIMESTAMP_HOUR',
+    'TIMESTAMP_MINUTE',
+    'TIMESTAMP_SECOND'
+    ];
+
+tagKeys = []
+for tag in arrayOutTags:
+    tagKeys.append(tag.split("{")[0]) # delete trailiing { if it exists
+
+#order matters!
+tagCmds = [
+    'LOAD_PROGRAM',
+    'START_PROGRAM',
+    'END_PROGRAM',
+    'ABORT_PROGRAM',
+    'RESET'];
+
+tagDateTime = [
+    'TIMESTAMP_MONTH',
+    'TIMESTAMP_DAY',
+    'TIMESTAMP_YEAR',
+    'TIMESTAMP_HOUR',
+    'TIMESTAMP_MINUTE',
+    'TIMESTAMP_SECOND'];
+
+tagStringInt = [
+    'PUN'];
+
+tagInt = [
+    'PART_TYPE',
+    'PART_PROGRAM'
+    ];
+
+tagChar = [
+    'MODULE',
+    'PLANTCODE'
+    ];
+
 def read_plc_dict(plc, machine_num):
     #print("read_plc_dict, generating list of read tags")
     readList = []
     for tag in arrayOutTags :
-        newTag = 'Program:HM1450_VS' + machine_num + '.VPC1.O.' + tag;
+        newTag = 'Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.O.' + tag;
         #print(newTag);
         readList.append(newTag)
         
@@ -144,83 +105,22 @@ def read_plc_dict(plc, machine_num):
 #Writing back to PLC to mirror data on LOAD
 #TODO: Modify to use running values instead of relying on reads from an OPC server
 def write_plc(plc, machine_num, results):
-
-    ''' 
-    #'PUN' and 'GMPartNumber' conversions are unnecessary after removing OPC server(?)
-    #the 'PUN' tags requires string-to-int64[] conversion to write to the PLC
-    #plc_PUN_int = []
-    plc_PUN_int = results['PUN'][1] #result directly from PLC results instead of OPC, no need to convert for writing back(?)
-    #print(plc_PUN_int)
-    #converts a string into an array of ASCII int64 values
-    plc_PUN_int = strArray_to_intArray(plc_PUN_int)
-    #print(plc_PUN_int)
-    #appending NULL values to fill out plc_PUN_int array[64]
-    for i in range(len(plc_PUN_int), 64):
-            #None = NULL in Python
-            #plc_PUN_int.append(None)
-            plc_PUN_int.append(0) #? Maybe won't write because 'None' is a different type than 'int'
-            #plc_PUN_int[i] = None
-
-    #requires 'I.GMPartNumber' to be set with 8-characters, part of the dotnet-opc-client's 'copy'
-    #plc_GMPartNumber= client.nodes.root.get_child(["0:Objects", f"{idx}:HM1450_VS" + machine_num, f"{idx}:VPC1.I.GMPartNumber"])
-    #plc.write('Program:HM1450_VS' + machine_num + '.VPC1.I.GMPartNumber', await plc_GMPartNumber.read_value())
-    plc_GMPartNumber_int = opc_variables['GMPartNumber_' + machine_num + 'i'].get_value()
-    #print(plc_GMPartNumber_int)
-    plc_GMPartNumber_int = strArray_to_intArray(plc_GMPartNumber_int)
-    #print(plc_GMPartNumber_int)
-    #print()
-    #plc.write('Program:HM1450_VS' + machine_num + '.VPC1.I.GMPartNumber{8}', plc_GMPartNumber_int)
-
-    plc_PUN_int = opc_snapshot[9]
-    #print(plc_PUN_int)
-    #converts a string into an array of ASCII int64 values
-    plc_PUN_int = strArray_to_intArray(plc_PUN_int)
-    #print(plc_PUN_int)
-    #appending NULL values to fill out plc_PUN_int array[64]
-    for i in range(len(plc_PUN_int), 64):
-            #None = NULL in Python
-            #plc_PUN_int.append(None)
-            plc_PUN_int.append(0) #? Maybe won't write because 'None' is a different type than 'int'
-            #plc_PUN_int[i] = None
-
-    plc_GMPartNumber_int = opc_snapshot[10]
-    #print(plc_GMPartNumber_int)
-    plc_GMPartNumber_int = strArray_to_intArray(plc_GMPartNumber_int)
-
-    #print(opc_snapshot)
-    #time.sleep(5)
-    '''
-
     #logging how long the 'read' takes
     start_time = datetime.datetime.now()
 
-    plc.write(('Program:HM1450_VS' + machine_num + '.VPC1.I.PartType', results['PartType'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.PartProgram', results['PartProgram'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.ScanNumber', results['ScanNumber'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.PUN{64}', results['PUN'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.GMPartNumber{8}', results['GMPartNumber'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.Module', results['Module'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.PlantCode', results['PlantCode'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.Month', results['Month'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.Day', results['Day'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.Year', results['Year'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.Hour', results['Hour'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.Minute', results['Minute'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.Second', results['Second'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP110', results['QualityCheckOP110'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP120', results['QualityCheckOP120'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP130', results['QualityCheckOP130'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP140', results['QualityCheckOP140'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP150', results['QualityCheckOP150'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP310', results['QualityCheckOP310'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP320', results['QualityCheckOP320'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP330', results['QualityCheckOP330'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP340', results['QualityCheckOP340'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP360', results['QualityCheckOP360'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP370', results['QualityCheckOP370'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP380', results['QualityCheckOP380'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckOP390', results['QualityCheckOP390'][1]),
-    ('Program:HM1450_VS' + machine_num + '.VPC1.I.QualityCheckScoutPartTracking', results['QualityCheckScoutPartTracking'][1])
+    plc.write(('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.PartType', results['PartType'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.PartProgram', results['PartProgram'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.ScanNumber', results['ScanNumber'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.PUN{64}', results['PUN'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.GMPartNumber{8}', results['GMPartNumber'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.Module', results['Module'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.PlantCode', results['PlantCode'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.Month', results['Month'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.Day', results['Day'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.Year', results['Year'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.Hour', results['Hour'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.Minute', results['Minute'][1]),
+    ('Program:CM080CA01.PorosityInspect.CAM0' + machine_num + '.I.Second', results['Second'][1])
     )
 
     end_time = datetime.datetime.now()
@@ -230,62 +130,6 @@ def write_plc(plc, machine_num, results):
     #print("Finished writing PLC...")
     pass
 #END write_plc
-
-#used to verify values when writing back to PLC
-def protected_ord(value):
-    if len(value) > 1:
-        print("WRONG Below: ")
-        print(value)
-        value = value[0]
-    #print("Returning:")
-    #print(value)
-    return ord(value)
-#END protected_ord
-
-#function to translate PLC int-arrays into ASCII str for OPC
-def intArray_to_str(intArray):
-    strReturn = ""
-    #print(intArray)
-
-    #reads each 'int' from array then appends to 'str' array in char form (per element)
-    for i in range(len(intArray)):
-        strReturn += chr(intArray[i])
-
-    #print(strReturn)
-    return strReturn
-#END intArray_to_str
-
-#function to translate PLC int-arrays into ASCII str-arrays for OPC
-def intArray_to_strArray(intArray):
-    #declaring an array of 'str' to hold return value
-    strArray = []
-    #print(intArray)
-
-    #print()
-
-    #reads each 'int' from array then appends to 'str' array in char form (per element)
-    for i in range(len(intArray)):
-        strArray.append(chr(intArray[i]))
-
-    #print(strArray)
-    return strArray
-#END intArray_to_strArray
-
-#function to translate str-arrays into PLC int-arrays
-def strArray_to_intArray(strArray):
-    #declaring an array of 'int' to hold return value
-    intArray = []
-    strArray_list = list(strArray)
-    #print(strArray_list)
-    #print(list(strArray))
-
-    #reads each char from str array then appends to 'intArray' in int form (per element)
-    for i in range(len(strArray_list)):
-        
-        intArray.append(ord(strArray_list[i]))
-
-    return intArray
-#END strArray_to_intArray
 
 # Triggering Keyence with socket only being connected/closed ONCE (program startup and shutdown)
 def TriggerKeyence(sock, item):
@@ -356,7 +200,7 @@ def main():
     global current_stage #keeps track of which stage program is currently in from the timing process
 
     print("Connecting to PLC")
-    with LogixDriver('120.57.42.114') as plc:
+    with LogixDriver('120.123.230.39/0') as plc:
         while(True):
             #reading in .csv plc emulator for initial values
             #split into two .csv files, representing each direction of traffic between PLC and Phoenix
@@ -364,21 +208,21 @@ def main():
             #csv_results_plc = csv_read('i') #holds PLC (Grob) tags / data
             csv_results = {}
 
-            results_14_dict = read_plc_dict(plc, '14') #initial PLC tag read for 'robot 14' values
-            #print(results_14_dict['LoadProgram'][1]) #how to print the value of one specific tag
+            results_14_dict = read_plc_dict(plc, '1') #initial PLC tag read for 'robot 14' values
+            print(results_14_dict['LOAD_PROGRAM'][1]) #how to print the value of one specific tag
 
             #STAGE0 CHECK HERE
             if(current_stage == 0):
                 print('Stage 0 : Listening for PLC(LOAD_PROGRAM) = 1')
                 #reading PLC until LOAD_PROGRAM goes high
-                while(results_14_dict['LoadProgram'][1] != True):
-                    results_14_dict = read_plc_dict(plc, '14') # continuous PLC read
+                while(results_14_dict['LOAD_PROGRAM'][1] != True):
+                    results_14_dict = read_plc_dict(plc, '1') # continuous PLC read
                     #print(csv_results)
                     time.sleep(1)
 
                 print('PLC(LOAD_PROGRAM) went high!')
                 # Once PLC(LOAD_PROGRAM) goes high, mirror data and set Phoenix(READY) high, signifies end of "loading" process
-                plc.write('Program:HM1450_VS14.VPC1.I.Ready', False)
+                plc.write('Program:CM080CA01.PorosityInspect.CAM01.I.READY', False)
                 print('Dropping Phoenix(READY) low.')
 
                 ''' 
@@ -397,7 +241,7 @@ def main():
                 12= Front Side(45T3) 
                 '''
                 #TODO Send branch data to load Keyence for scan
-                LoadKeyence(sock,'MW,#PhoenixControlFaceBranch,' + str(results_14_dict['PartProgram'][1]) + '\r\n') #Keyence loading message, uses PartProgram from PLC to load specific branch
+                LoadKeyence(sock,'MW,#PhoenixControlFaceBranch,' + str(results_14_dict['PART_PROGRAM'][1]) + '\r\n') #Keyence loading message, uses PartProgram from PLC to load specific branch
                 print('Keyence Loaded!')
 
                 #TODO Actually Mirror Data (write back to PLC)
@@ -406,7 +250,7 @@ def main():
                 #csv_results['DATA'] = csv_results_plc['DATA']
                 time.sleep(1) #artificial pause to see step happening in testing
                 print('Data Mirrored, Setting \'READY\' high')
-                plc.write('Program:HM1450_VS14.VPC1.I.Ready', True)
+                plc.write('Program:CM080CA01.PorosityInspect.CAM01.I.READY', True)
                 time.sleep(3)
                 current_stage += 1 #incrementing out of STAGE0
             #END STAGE0
@@ -414,20 +258,20 @@ def main():
             elif(current_stage == 1):
                 print('Stage 1!\nListening for PLC(START_PROGRAM) = 1')
                 time.sleep(.01) #10ms artificial delay for testing
-                if(results_14_dict['StartProgram'][1] == True):
+                if(results_14_dict['START_PROGRAM'][1] == True):
                     print('PLC(START_PROGRAM) went high! Time to trigger Keyence...')
-                    plc.write('Program:HM1450_VS14.VPC1.I.Busy', True) #Busy goes HIGH while Keyence is scanning
+                    plc.write('Program:CM080CA01.PorosityInspect.CAM01.I.BUSY', True) #Busy goes HIGH while Keyence is scanning
 
                     #Actual Keyence Trigger (T1) here***
                     TriggerKeyence(sock, 'T1\r\n')
 
                     print('Pretend scan ended! PHOENIX(BUSY) is low')
-                    plc.write('Program:HM1450_VS14.VPC1.I.Busy', False)
+                    plc.write('Program:CM080CA01.PorosityInspect.CAM01.I.BUSY', False)
 
                     #TODO PASS/FAIL RESULTS
                     print('PASS/FAIL/DONE data written out')
-                    plc.write(('Program:HM1450_VS14.VPC1.I.Pass', True),
-                    'Program:HM1450_VS14.VPC1.I.Done', True)
+                    plc.write(('Program:CM080CA01.PorosityInspect.CAM01.I.PASS', True),
+                    'Program:CM080CA01.PorosityInspect.CAM01.I.DONE', True)
                     print('PHOENIX(PASS) and PHOENIX(DONE) = 1')
                     print('Stage 1 Complete!')
                     current_stage += 1
@@ -435,13 +279,13 @@ def main():
                 
             #Final Stage, reset to Stage 0 once PLC(END_PROGRAM) and PHOENIX(DONE) have been set low
             elif(current_stage == 2):
-                done_check = plc.read('Program:HM1450_VS14.VPC1.I.Done')
+                done_check = plc.read('Program:CM080CA01.PorosityInspect.CAM01.I.DONE')
                 print('Stage 2 : Listening to PLC(END_PROGRAM) low to reset back to Stage 0')
-                if(results_14_dict['EndProgram'][1] == True):
+                if(results_14_dict['END_PROGRAM'][1] == True):
                     print('PLC(END_PROGRAM) is high. Dropping PHOENIX(DONE) low')
-                    plc.write('Program:HM1450_VS14.VPC1.I.Done', False)
+                    plc.write('Program:CM080CA01.PorosityInspect.CAM01.I.DONE', False)
                     
-                if(results_14_dict['EndProgram'][1] == False and done_check[1] == False):
+                if(results_14_dict['END_PROGRAM'][1] == False and done_check[1] == False):
                     print('PLC(END_PROGRAM) is low. Resetting PHOENIX to Stage 0')
                     current_stage = 0 # cycle complete, reset to stage 0
                 
